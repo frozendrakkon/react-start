c94a9c34e32af42f1746b7e283bd2e622282d4ff
frozendrakon <frozendrakon@gmail.com>
2021-02-10 17:31:14 +0300

    pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Header />
+         Hello world!
+          <Tech />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
-export default App;
+const Tech = (() => {
+  return(
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  )
+})
+
+
+const Header = () => {
+  return(
+    <div>
+      <a href="#">Serega</a>
+      <a href="#">Love</a>
+      <a href="#">Death</a>
+    </div>
+  )
+}
+
+export default App; // экспортируем компоненту
+
