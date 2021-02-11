c804350dab489afdf5b704ca7eaa3678b35f4086
frozendrakon <frozendrakon@gmail.com>
2021-02-11 15:16:57 +0300

    social web start

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-        <p>
-          <Header />
-         Hello world!
-          <Tech />
-        </p>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://i.pinimg.com/originals/c2/61/eb/c261eb4f5b4d38cf4f320f9188430c41.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Message</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" />
+        </div>
+        <div>
+          ava + descr
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
-
-const Tech = (() => {
-  return(
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
-  )
-})
-
-
-const Header = () => {
-  return(
-    <div>
-      <a href="#">Serega</a>
-      <a href="#">Love</a>
-      <a href="#">Death</a>
-    </div>
   )
 }
-
-export default App; // экспортируем компоненту
-
+export default App
