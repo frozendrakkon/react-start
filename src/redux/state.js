b6a2efb34c12e9adc67090e1e67f6bdfee39e3be
frozendrakon <frozendrakon@gmail.com>
2021-03-12 18:05:45 +0300

    _

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,6 @@
-const ADD_POST = "ADD_POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE_NEW_POST-TEXT";;
-
-const UPDATE_NEW_POST_BODY = "UPDATE_NEW_POST_BODY";
-const SEND_MESSAGE = "SEND_MESSAGE";
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
 
 let store = {
     _state: {
@@ -30,7 +28,8 @@ let store = {
                 { name: "Mark", id: 5 }
             ],
             newMessageText: ""
-        }
+        },
+        sidebar: {}
     },
 
     _callSubscriber() {
@@ -46,52 +45,18 @@ let store = {
         this._callSubscriber = observer
     },
 
-    dispatch(action) { // !! {type : "ADD-POSt"}
-        if (action.type === ADD_POST) {
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
+    dispatch(action) {
 
-            this._state.profilePage.postData.push(newPost);
-            this._state.profilePage.newPostText = ""
-            this._callSubscriber(this._state)
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.messagesPage = dialogsReducer(this._state.messagesPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
-        } else if (action.type === UPDATE_NEW_POST_TEXT) {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state)
-        } else if (action.type === UPDATE_NEW_POST_BODY) {
-            this._state.messagesPage.newMessageText = action.body;
-        } else if (action.type === SEND_MESSAGE) {
-            let body = this._state.messagesPage.newMessageText;
-            this._state.messagesPage.newMessageText = ""
-            this._state.messagesPage.messages.push({ id: 6, message: body  });
-            debugger
-            this._callSubscriber(this._state)
-        }
+        this._callSubscriber(this._state)
+
     }
 
 };
 
-export const addPostActionCreator = () => ({
-    type: ADD_POST
-})
 
-export const updateNewPostTextActionCreator = (text) => ({
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text
-})
-
-
-export const sendMessageCreator = () =>
-({
-    type: SEND_MESSAGE
-})
-
-export const updateNewMessageBodyCreator = (body) => ({
-    type: UPDATE_NEW_POST_BODY,
-    body: body
-})
 
 export default store
